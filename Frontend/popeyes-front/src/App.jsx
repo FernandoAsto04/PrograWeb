cb35c6c4358a372aeac09f833b17a26865032a35
Vale <20211854@aloe.ulima.edu.pe>
2024-11-09 22:04:32 -0500

    header sin componentes
    Me faltan las imagenes

--- Frontend/popeyes-front/src/App.jsx
+++ Frontend/popeyes-front/src/App.jsx
@@ -1,10 +1,11 @@
 import { Header } from './modules/widgets/components/Header.jsx'
-
+import './App.css'
 function App() {
 
   return (
     <>
       <Header/>
+      <p>Alo</p>
     </>
   )
 }
--- Frontend/popeyes-front/src/modules/widgets/components/Header.jsx
+++ Frontend/popeyes-front/src/modules/widgets/components/Header.jsx
@@ -1,30 +1,75 @@
 import React from "react";
+import { Link, Route, BrowserRouter as Router, Routes, useParams } from 'react-router-dom';
+
+
+const Pedidos = () => {
+    return(
+        <>
+
+        </>
+    );
+  }
 
 
 export const Header = () => {
+    
     return(
-        <nav className="nav">
-            <div className="container-img">
-                <a href="/">
-                    <img id="logo" src= "/logo.png" alt="Logo" /> 
-                </a>
-            </div>
-            
-            <div className="container-texto">
-                <ul>
-                        <li>
-                            <a href="/Menu">Menu</a>
-                        </li>
-                        <li>
-                            <a href="/pedidos">Mis pedidos</a>
-                        </li>
-                        <li>
-                            <a href="/locales">Locales</a>
-                        </li>
+        <Router> 
+            <nav className="nav">
+                <div className="container-img">
+                    <Link to='/'>
+                        <img id="logo" src= "/logo.png" alt="Logo" /> 
+                    </Link>
+                </div>
+
+                <div className="container-boton">
+                    <button> <Link to='/pedido-delivery'> ¿Cómo deseas recibir tu pedido? </Link></button>
+                </div>
+                
+                <div className="container-texto">
+                    <ul>
+                            <li>
+                                <Link to="/menu">Menu</Link>
+                            </li>
+                            <li>
+                                <Link to="/pedidos">Mis pedidos</Link>
+                            </li>
+                            <li>
+                                <Link to="/locales">Locales</Link>
+                            </li>
+                    </ul>
+                </div>
+
+                <div className="container-extra">
+
+                    <div className="extra">
+                        <Link to='/contacto'>
+                            Llámanos
+                            <br></br>
+                            <strong className="orange-text">631-6000</strong>
+                        </Link>
+                    </div>
                     
-                </ul>
-            </div>
-            
-        </nav>
+                    <div className="extra-s">
+                        <Link to='/login'>
+                            Hola,
+                            <br></br>
+                            <strong className="orange-text">Iniciar Sesión</strong>
+                        </Link>
+                    </div>
+
+                    <div className="extra-c">
+                        <Link to='/shopping-cart'>
+                            <strong className="orange-text">S/.0.00</strong>
+                        </Link>
+                    </div>
+                </div>
+
+                <Routes>
+                    <Route path="/"></Route>
+                    <Route path="/pedido-delivery" element={<Pedidos></Pedidos>}></Route>
+                </Routes>
+            </nav>
+        </Router>
     )
 }
