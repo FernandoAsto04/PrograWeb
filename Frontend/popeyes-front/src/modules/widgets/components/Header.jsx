255f4a8f781fcb261b62139e97bdf651f142fb73
Vale <20211854@aloe.ulima.edu.pe>
2024-11-12 17:11:05 -0500

    footer

--- Frontend/popeyes-front/src/modules/widgets/components/Header.jsx
+++ Frontend/popeyes-front/src/modules/widgets/components/Header.jsx
@@ -15,62 +15,61 @@ export const Header = () => {
     
     return(
         <Router> 
-            <nav className="nav">
-                <div className="container-img">
-                    <Link to='/'>
-                        <img id="logo" src= "/logo.png" alt="Logo" /> 
-                    </Link>
-                </div>
-
-                <div className="container-boton">
-                    <button> <Link to='/pedido-delivery'> ¿Cómo deseas recibir tu pedido? </Link></button>
-                </div>
-                
-                <div className="container-texto">
-                    <ul>
-                            <li>
-                                <Link to="/menu">Menu</Link>
-                            </li>
-                            <li>
-                                <Link to="/pedidos">Mis pedidos</Link>
-                            </li>
-                            <li>
-                                <Link to="/locales">Locales</Link>
-                            </li>
-                    </ul>
-                </div>
-
-                <div className="container-extra">
-
-                    <div className="extra">
-                        <Link to='/contacto'>
-                            Llámanos
-                            <br></br>
-                            <strong className="orange-text">631-6000</strong>
+                <nav className="nav">
+                    <div className="container-img">
+                        <Link to='/'>
+                            <img id="logo" src= "/logo.png" alt="Logo" /> 
                         </Link>
+                    </div>
+
+                    <div className="container-boton">
+                        <button> <Link to='/pedido-delivery'> ¿Cómo deseas recibir tu pedido? </Link></button>
                     </div>
                     
-                    <div className="extra-s">
-                        <Link to='/login'>
-                            Hola,
-                            <br></br>
-                            <strong className="orange-text">Iniciar Sesión</strong>
-                        </Link>
+                    <div className="container-texto">
+                        <ul>
+                                <li>
+                                    <Link to="/menu">Menu</Link>
+                                </li>
+                                <li>
+                                    <Link to="/pedidos">Mis pedidos</Link>
+                                </li>
+                                <li>
+                                    <Link to="/locales">Locales</Link>
+                                </li>
+                        </ul>
                     </div>
 
-                    <div className="extra-c">
-                        <Link to='/shopping-cart'>
-                            <strong className="orange-text">S/.0.00</strong>
-                        </Link>
-                    </div>
-                </div>
+                    <div className="container-extra">
 
-                <Routes>
-                    <Route path="/"></Route>
-                    <Route path="/pedido-delivery" element={<Pedidos></Pedidos>}></Route>
-                </Routes>
-            </nav>
-            
+                        <div className="extra">
+                            <Link to='/contacto'>
+                                Llámanos
+                                <br></br>
+                                <strong className="orange-text">631-6000</strong>
+                            </Link>
+                        </div>
+                        
+                        <div className="extra-s">
+                            <Link to='/login'>
+                                Hola,
+                                <br></br>
+                                <strong className="orange-text">Iniciar Sesión</strong>
+                            </Link>
+                        </div>
+
+                        <div className="extra-c">
+                            <Link to='/shopping-cart'>
+                                <strong className="orange-text">S/.0.00</strong>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <Routes>
+                        <Route path="/"></Route>
+                        <Route path="/pedido-delivery" element={<Pedidos></Pedidos>}></Route>
+                    </Routes>
+                </nav>
         </Router>
     )
 }
