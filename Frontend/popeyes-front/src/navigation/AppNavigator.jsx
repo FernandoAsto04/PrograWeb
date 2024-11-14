5612c34eb452fef8d9048d85381c43db3eb53bfc
mlandaf <marcelo.landa72@gmail.com>
2024-11-13 21:47:08 -0500

    Navigation_y_Admin
    Se agrego el appnavigator.jsx para hacer las rutas desde ahi
    Se agrego la parte de administrador

---
+++ Frontend/popeyes-front/src/navigation/AppNavigator.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
+//IMPORT - COMPONENTES
+import LoginScreen from '../modules/auth/screens/LoginScreen'
+import MisPedidosScreen from '../modules/mispedidos/screens/MisPedidosScreen'
+import LocalesScreen from '../modules/locales/screens/LocalesScreen'
+import MenuScreen from '../modules/menu/screens/MenuScreen'
+import Principal from '../modules/X_Principal'
+import Pruebas from '../modules/X_Pruebas'
+import DashboardScreen from '../modules/admin/screens/DashboardScreem';
+
+function AppNavigator() {
+    return (
+        <Routes>
+            <Route path='/' element={<Principal/>}/>
+            <Route path='/pruebas' element={<Pruebas/>}/>  
+            <Route path='/login' element={<LoginScreen/>}/>
+            <Route path='/mispedidos' element={<MisPedidosScreen/>}/>
+            <Route path='/locales' element={<LocalesScreen/>}/>
+            <Route path='/menu' element={<MenuScreen/>}/>
+            <Route path='/admin' element={<DashboardScreen/>}/>
+        </Routes>
+    );
+  }
+  
+  export default AppNavigator;
