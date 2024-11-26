f92515cf85388ebc7a03805124289379a8161b54
mlandaf <marcelo.landa72@gmail.com>
2024-11-25 23:18:41 -0500

    Update AppNavigator.jsx

--- Frontend/popeyes-front/src/navigation/AppNavigator.jsx
+++ Frontend/popeyes-front/src/navigation/AppNavigator.jsx
@@ -9,8 +9,8 @@ import MenuScreen from '../modules/menu/screens/MenuScreen'
 import Principal from '../modules/X_Principal'
 import Pruebas from '../modules/X_Pruebas'
 import DashboardScreen from '../modules/admin/screens/DashboardScreem';
-import Locales_Admin from '../modules/admin/screens/Locales_Admin'
-import Combos_Admin from '../modules/admin/screens/Combos_Admin'
+import Locales_Admin from '../modules/admin/screens/Locales_Admin';
+import Combos_Admin from '../modules/admin/screens/Combos_Admin';
 
 function AppNavigator() {
     return (
