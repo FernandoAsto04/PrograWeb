6f628f658dfee6c26c48feb9fc277945c68da7c9
Frank Tapia <20214234@aloe.ulima.edu.pe>
2024-12-01 20:07:09 -0500

    Conexion Fro Back de prueba de Combo

--- Frontend/popeyes-front/src/modules/x_froback_tests/services/14_ComboServices.js
+++ Frontend/popeyes-front/src/modules/x_froback_tests/services/14_ComboServices.js
@@ -8,3 +8,18 @@ export const obtenerCombos = async () => {
         console.error("Error al obtener combos");
     }
 };
+
+export const crearCombo = async (combo) => {
+    try {
+        const response = await fetch(API_URL,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(combo),
+        })
+        return await response.json();
+    }catch(error){
+        console.error("Error al crear combo");
+    }
+}
